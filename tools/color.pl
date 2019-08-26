9f762f9c3221fd71022f4fd1c22f99e64154e210
michiharu-kondo <michiharu-kondo@flight.co.jp>
2019-08-26 10:05:51 +0900

    カラー表示ツールを更新

--- tools/color.pl
+++ tools/color.pl
@@ -11,6 +11,15 @@ for( my $i=0; $i<256; $i++ )
 	}
 }
 
+printf( "\n" );
 
+for( my $i=0; $i<256; $i++ )
+{
+	printf( "\e[38;5;%dm %03d \e[m", $i, $i );
 
+	my $n = ($i+1) % 10;
+	if ( $n == 0 ) {
+		printf( "\n" );
+	}
+}
 
